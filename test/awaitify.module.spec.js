1363a70fee60f3dea7ad45210eb7aee3332d4ee3
Mohamad Reza Lotfi Namin <mohamadmlnnamin@gmail.com>
2017-12-17 10:36:02 +0330

    fixed wrapInGenRunCallback option and added tests

--- test/awaitify.module.spec.js
+++ test/awaitify.module.spec.js
@@ -1,8 +1,9 @@
 const chai = require('chai');
+const isPromiseAlike = require('../lib/isPromiseAlike');
 const awaitify = require('../index');
 const expect = chai.expect;
 const it = awaitify.it(global.it);
-const { SampleModuleV1, SampleModuleV2 } = require('./module-samples');
+const { SampleModuleV1, SampleModuleV2, SampleModuleV3 } = require('./module-samples');
 
 const sampleModule = {
   *doSomething(){
@@ -16,10 +17,12 @@ describe('awaitify', () => {
   describe('#module()', () => {expect(SampleModuleV2.helloWorld()).to.equal('helloWorld v1');
     let resultV1;
     let resultV2;
+    let resultV3;
 
     function * sampleModules() {
       resultV1 = SampleModuleV1.createSomething();
       resultV2 = SampleModuleV2.createSomething();
+      resultV3 = SampleModuleV3.createSomething();
     }
 
     beforeEach(function(done) {
@@ -30,9 +33,13 @@ describe('awaitify', () => {
         });
     });
 
-    it('an async function should return a gen-run callback', () => {
+    it('an async function should return a gen-run callback without doNotWrapInGenRun option', () => {
       expect(resultV1).to.be.a('function');
       expect(resultV2).to.be.a('function');
+    });
+
+    it('an async function should return a Promise with doNotWrapInGenRun', function* () {
+      expect(isPromiseAlike(resultV3)).to.be.true;
     });
 
     it('should automatically convert a generator function to an async function #1', function* () {
@@ -45,6 +52,12 @@ describe('awaitify', () => {
       const result = yield resultV2;
       expect(result).to.be.a('string');
       expect(result).to.equal('Something V1, V2');
+    });
+
+    it('should automatically convert a generator function to an async function #3', function* () {
+      const result = yield resultV3;
+      expect(result).to.be.a('string');
+      expect(result).to.equal('Something V1, V3');
     });
 
     it('should be able to inherit values and methods from a base module', () => {
